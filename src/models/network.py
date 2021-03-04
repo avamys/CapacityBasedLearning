ffcd53c3611b0cf9329cceb6095b3c1b4799cc10
avamys <dominik.grzegorzewicz@gmail.com>
2021-03-04 05:28:41 +0100

    Split lipschitz update and fix problems with updating params

--- src/models/network.py
+++ src/models/network.py
@@ -7,9 +7,9 @@ import torch.nn.init as init
 from torch import Tensor
 
 from src.models.utils import get_activation
-from typing import List
+from typing import List, Tuple
 from collections import deque
-
+from scipy.special import binom
 
 class Model(nn.Module):
     ''' Simple parametrized network '''
@@ -67,8 +67,9 @@ class BuddingLayer(nn.Module):
         super().__init__()
 
         self.window_size = window_size
+        self.lipschitz_size = int(binom(self.window_size+1, 2))
         self.weights_window = deque(maxlen=self.window_size)
-        self.lipshitz_constants = deque(maxlen=self.window_size)
+        self.lipshitz_constants = deque(maxlen=self.lipschitz_size)
         self.saturated_neurons = torch.zeros(size_in, dtype=torch.bool)
         
         self.size_in = size_in
@@ -79,9 +80,9 @@ class BuddingLayer(nn.Module):
             self.bias = nn.Parameter(Tensor(size_out))
         else:
             self.register_parameter('bias', None)
-        self.reset_parameters()
+        self.__reset_parameters()
 
-    def reset_parameters(self) -> None:
+    def __reset_parameters(self) -> None:
         ''' Weights and biases initialization same as in basic nn.Linear '''
         init.kaiming_uniform_(self.weight, a=math.sqrt(5))
         if self.bias is not None:
@@ -89,75 +90,65 @@ class BuddingLayer(nn.Module):
             bound = 1 / math.sqrt(fan_in)
             init.uniform_(self.bias, -bound, bound)
 
-    def update_cache(self):
+    def __update_cache(self) -> None:
         ''' Adds weights to the cache '''
-        self.weights_window.append(self.state_dict()['weight'].clone().detach())
+        current_weights = self.state_dict()['weight']
 
-    def get_lipschitz_constant(self):
-        ''' Compute lipshitz constant for any new epoch '''
+        self.weights_window.append(current_weights.clone().detach())
+
+    def __update_lipschitz_cache(self) -> None:
+        ''' Adds lipschitz constants computed for a current epoch's weights 
+            to the cache.
+        '''
         current_weights = self.state_dict()['weight']
 
         for epoch_id, cached_weights in enumerate(self.weights_window):
             dist_func = torch.abs(torch.sum(cached_weights, dim=1) - torch.sum(current_weights, dim=1))
-            dist_points = 1 if self.window_size - epoch_id == 1 else (self.window_size - epoch_id) - 1
+            dist_points = self.window_size - epoch_id
 
             self.lipshitz_constants.append(dist_func / dist_points)
 
-        return torch.max(torch.stack(list(self.lipshitz_constants), dim=1), dim=1)
-    
-    def forward(self, x: Tensor, saturated: Tensor) -> Tensor:
+    def get_lipschitz_constant(self) -> Tensor:
+        ''' Computes current best lipshitz constant or returns None if the 
+            window is not full 
+        '''
+
+        if len(self.lipshitz_constants) == self.lipschitz_size:
+            return torch.max(torch.stack(list(self.lipshitz_constants), dim=1), dim=1)[0]
+        return None
+
+    def forward(self, x: Tensor, saturated: Tensor = None) -> Tuple:
         ''' saturated tensor should be provided from the previous layer
 
         '''
+        self.__update_lipschitz_cache() # update lipschitz deque with current weights
+        self.__update_cache() # update weights deque with current weights
+
+        best_lipschitz_constant = self.get_lipschitz_constant() 
         u = 0
-        sat = torch.logical_or(self.saturated_neurons, saturated)
-        self.update_cache()
 
-        if sat.any().item() > 0:
-            # Add new NeuronBuds if the number of buds has changed
-            if torch.sum(sat) > torch.sum(self.saturated_neurons):
-                u_neurons = torch.nonzero(sat).view(-1).tolist()
-                u_buds = [int(key) for key in self.buds.keys()]
-                
-                for new_bud in list(set(u_neurons) - set(u_buds)):
-                    self.buds[str(new_bud)] = NeuronBud(self.size_out, 'relu') # To parametrize
+        if saturated is not None:
+            sat = torch.logical_or(self.saturated_neurons, saturated)
 
-                self.saturated_neurons = sat
+            if sat.any().item() > 0:
+                # Add new NeuronBuds if the number of buds has changed
+                if torch.sum(sat) > torch.sum(self.saturated_neurons):
+                    u_neurons = torch.nonzero(sat).view(-1).tolist()
+                    u_buds = [int(key) for key in self.buds.keys()]
+                    
+                    for new_bud in list(set(u_neurons) - set(u_buds)):
+                        self.buds[str(new_bud)] = NeuronBud(self.size_out, 'relu') # To parametrize
 
-            # Forward pass through buds
-            u = [self.buds[key](x[:, int(key)].view(-1,1)) for key in self.buds]
-            u = torch.stack(u, dim=1)
-            u = torch.sum(u, dim=1)
+                    self.saturated_neurons = sat
 
-            # Apply linear transformation for non-saturated neurons
-            x = x * (~self.saturated_neurons).float()
+                # Forward pass through buds
+                u = [self.buds[key](x[:, int(key)].view(-1,1)) for key in self.buds]
+                u = torch.stack(u, dim=1)
+                u = torch.sum(u, dim=1)
+
+                # Apply linear transformation for non-saturated neurons
+                x = x * (~self.saturated_neurons).float()
             
         x = F.linear(x, self.weight, self.bias)
 
-        return x + u
-
-class CapacityNet(nn.Module):
-    def __init__(self, in_features: int, out_size: int, layers: List[int],
-                 activation_name: str, activation_out: str, window_size):
-        super().__init__()
-
-        self.window_size = window_size
-
-        # activation_method = get_activation(activation_name)
-        # activation_out = get_activation(activation_out)
-        n_in = in_features
-
-        self.bl1 = BuddingLayer(n_in, 10, self.window_size)
-        self.bl2 = BuddingLayer(10, 10, self.window_size)
-        self.blout = BuddingLayer(10, out_size, self.window_size, False)
-
-        self.sat0 = torch.zeros(n_in, dtype=torch.bool)
-
-    def forward(self, x) -> Tensor:
-        x = F.relu(self.bl1(x, self.sat0))
-        saturation = self.bl1.get_lipschitz_constant()[0] > 0.1
-        x = F.relu(self.bl2(x, saturation))
-        saturation = self.bl2.get_lipschitz_constant()[0] > 0.01
-        x = self.blout(x, saturation)
-
-        return x
+        return x + u, best_lipschitz_constant
--- tests/test_models_network.py
+++ tests/test_models_network.py
@@ -2,45 +2,75 @@ import numpy as np
 import unittest
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from src.models.network import NeuronBud, BuddingLayer
 
 class TestBuddingLayer(unittest.TestCase):
     def setUp(self):
-        self.model = BuddingLayer(2, 3, 3)
-        self.bud = BuddingLayer(2, 2, 2)
-
+        self.bud = BuddingLayer(2, 2, 1)
         self.bud.weight = nn.Parameter(torch.ones((2,2)))
-        self.bud.update_cache()
-        self.bud.weight = nn.Parameter(torch.ones((2,2)) * 2)
-        self.bud.update_cache()
+        self.bud.bias = nn.Parameter(torch.ones(2))
 
-    def test_buds(self):
-        x = torch.tensor([[0.1, 0.1], [1.0, 1.0]])
-        saturated = torch.BoolTensor([True, False])
+        self.x = torch.tensor([[0.1, 0.1], [1.0, 1.0]])
+        self.saturated = torch.BoolTensor([False, True])
 
-        self.assertEqual(len(self.model.buds), 0)
-        
-        out = self.model(x, saturated)
-        
-        self.assertEqual(len(self.model.buds), 1)
-        self.assertEqual(list(self.model.buds.keys())[0], '0')
-        self.assertEqual(out.size()[0], 2)
-        self.assertEqual(out.size()[1], 3)
+    def test_buds_creation(self):
+        self.assertEqual(len(self.bud.buds), 0)
+        y, lip = self.bud(self.x, self.saturated)
+
+        self.assertEqual(len(self.bud.buds), 1)
+        self.assertEqual(list(self.bud.buds.keys())[0], '1')
+        self.assertEqual(y.size()[0], 2)
+        self.assertEqual(y.size()[1], 2)
+
+    def test_basic_forward(self):
+        y, lip = self.bud.forward(self.x)
+
+        self.assertEqual(len(self.bud.weights_window), 1)
+        self.assertEqual(len(self.bud.lipshitz_constants), 0)
+        self.assertIsNone(lip)
+
+        expected_y = F.linear(self.x, self.bud.weight, self.bud.bias)
+        self.assertTrue(torch.equal(y, expected_y))
+
+    def test_forward_with_bud(self):
+        y, lip = self.bud.forward(self.x, self.saturated)
+
+        u = self.bud.buds['1'](self.x[:, 1].view(-1, 1))
+        zero_x = self.x * (~self.saturated).float()
+        self.assertTrue(torch.equal(zero_x, torch.tensor([[0.1, 0], [1.0, 0]])))
+
+        clear_x = F.linear(zero_x, self.bud.weight, self.bud.bias)
+        expected_y = clear_x + u
+
+        self.assertTrue(torch.equal(y, expected_y))
 
     def test_window_cap(self):
-        self.assertIsNot(self.bud.weights_window[0], self.bud.weights_window[1])
-        self.assertEqual(len(self.bud.weights_window), 2)
-        self.bud.weight = nn.Parameter(torch.ones((2,2)))
-        self.bud.update_cache()
-        self.assertEqual(len(self.bud.weights_window), 2)
+        model = BuddingLayer(2, 2, 2)
+        model.weight = nn.Parameter(torch.ones((2,2)))
+        self.assertEqual(len(model.weights_window), 0)
+
+        y, lip = model.forward(self.x, self.saturated)
+        self.assertEqual(len(model.weights_window), 1)
+
+        model.weight = nn.Parameter(torch.ones((2,2)) * 2)
+        self.assertIsNot(model.weights_window[0], model.state_dict()['weight'])
+
+        y, lip = model.forward(self.x, self.saturated)
+        self.assertEqual(len(model.weights_window), 2)
+        self.assertIsNot(model.weights_window[0], model.weights_window[1])
 
     def test_lipschitz(self):
-        lip = self.bud.get_lipschitz_constant()[0]
-        lip_self = self.bud.lipshitz_constants[-1]
+        lip = self.bud.get_lipschitz_constant()
+        self.assertIsNone(lip)
 
-        self.assertEqual(len(self.bud.lipshitz_constants), 2)
-        self.assertTrue(torch.equal(lip_self, torch.tensor([0.0, 0.0])))
+        y, lip = self.bud.forward(self.x, self.saturated)
+        self.bud.weight = nn.Parameter(torch.ones((2,2)) * 2)
+
+        y, lip = self.bud.forward(self.x, self.saturated)
+
+        self.assertEqual(len(self.bud.lipshitz_constants), 1)
         self.assertTrue(torch.equal(lip, torch.tensor([2.0, 2.0])))
 
 
