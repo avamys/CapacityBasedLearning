27836675f522bbfafbe3f720938a1665abc3d5f7
avamys <dominik.grzegorzewicz@gmail.com>
2021-05-27 00:17:39 +0200

    Add 3 types of datasets and refactor preprocessing pipeline

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -4,62 +4,63 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
+from typing import Tuple, Union
 
 import src.data.utils as utils
 
+Dataset = Tuple[pd.DataFrame, Union[pd.DataFrame, pd.Series]]
 
 class DataPreprocessor:
     ''' Class for reading dataset file from a given path, processing it
         properly and saving to given path as a csv file.
     '''
-    def __init__(self):
-        self.X, self.y = pd.DataFrame(), pd.DataFrame()
-        self.dataset_name = str()
+    def __init__(self, name: str):
+        self.dataset_name = name
 
-    def read_data(self, path: str):
+    def read_data(self, path: str) -> Dataset:
         ''' Reads raw data into DataProcessor '''
 
-        # Pull out the dataset name from the path
-        self.dataset_name = path.split('/')[-1].split('.')[0]
-        preprocessing_function = utils.get_preprocessing_function(self.dataset_name)
-        self.X, self.y = preprocessing_function(path)
+        utils.get_dataset(self.dataset_name, path)
 
-    def preprocess_data(self):
+        preprocessing_function = utils.get_preprocessing_function(self.dataset_name)
+        return preprocessing_function(path)
+
+    def preprocess_data(self, X: pd.DataFrame, y: pd.Series) -> Dataset:
         ''' Preprocess data using defined pipelines for categorical and
             numerical features
         '''
-        if self.dataset_name == 'drugs':
-            self.X = self.X.to_numpy()
-        else:
-            # Pipeline for numeric columns
-            numeric = self.X.loc[:, (self.X.dtypes == np.float64) | (self.X.dtypes == np.int64) ].columns
-            numeric_pipeline = Pipeline(steps=[
-                ('imputer', SimpleImputer(strategy='median')),
-                ('scaler', StandardScaler())])
 
-            # Pipeline for categorical columns
-            categorical = self.X.loc[:, self.X.dtypes == object].columns
-            categorical_pipeline = Pipeline(steps=[
-                ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
-                ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+        # Pipeline for numeric columns
+        numeric = X.loc[:, (X.dtypes == np.float64) | (X.dtypes == np.int64) ].columns
+        numeric_pipeline = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median')),
+            ('scaler', StandardScaler())])
 
-            preprocessor = ColumnTransformer(
-                transformers=[
-                    ('numeric', numeric_pipeline, numeric),
-                    ('categorical', categorical_pipeline, categorical)])
+        # Pipeline for categorical columns
+        categorical = X.loc[:, X.dtypes == object].columns
+        categorical_pipeline = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
-            self.X = preprocessor.fit_transform(self.X)
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('numeric', numeric_pipeline, numeric),
+                ('categorical', categorical_pipeline, categorical)])
+
+        X = preprocessor.fit_transform(X)
 
         # Label encoding for potential target variables
         le = LabelEncoder()
-        self.y = pd.Series(le.fit_transform(self.y), name='class')
+        y = pd.Series(le.fit_transform(y), name='class')
 
-    def write_data(self, path: str):
+        return X, y
+
+    def write_data(self, path: str, X: pd.DataFrame, y: pd.Series) -> None:
         ''' Saves X and y as csv files in given directory '''
 
         # In case the ndarray was converted to sparse
-        if type(self.X) != np.ndarray:
-            self.X = self.X.toarray()
+        if type(X) != np.ndarray:
+            X = X.toarray()
 
-        np.savetxt(path+self.dataset_name+'_target'+'.csv', self.y, delimiter=",")
-        np.savetxt(path+self.dataset_name+'_features'+'.csv', self.X, delimiter=",")
+        np.savetxt(path+self.dataset_name+'_target'+'.csv', y, delimiter=",")
+        np.savetxt(path+self.dataset_name+'_features'+'.csv', X, delimiter=",")
