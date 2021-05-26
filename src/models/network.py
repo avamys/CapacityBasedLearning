Fix lipschitz constant calculation and minor tests bugs

src/models/network.py
@@ -193,7 +193,7 @@ class BuddingLayer(nn.Module):
         current_weights = self.state_dict()['weight']
 
         for epoch_id, cached_weights in enumerate(self.weights_window):
-            dist_func = torch.sum(cached_weights, dim=1) - torch.sum(current_weights, dim=1)
+            dist_func = torch.sum(current_weights, dim=1) - torch.sum(cached_weights, dim=1)
             dist_points = self.window_size - epoch_id
 
             self.lipshitz_constants.append(dist_func / dist_points)

tests/test_models_network.py
@@ -8,7 +8,13 @@ from src.models.network import NeuronBud, BuddingLayer
 
 class TestBuddingLayer(unittest.TestCase):
     def setUp(self):
-        self.bud = BuddingLayer(2, 2, 1)
+        self.params = {
+            'size_in': 3,
+            'threshold': 0.001,
+            'layers': [3,3],
+            'activation': 'tanh'
+        }
+        self.bud = BuddingLayer(2, 2, 1, self.params)
         self.bud.weight = nn.Parameter(torch.ones((2,2)))
         self.bud.bias = nn.Parameter(torch.ones(2))
 
@@ -49,7 +55,7 @@ class TestBuddingLayer(unittest.TestCase):
         self.assertTrue(torch.equal(y, expected_y))
 
     def test_window_cap(self):
-        model = BuddingLayer(2, 2, 2)
+        model = BuddingLayer(2, 2, 2, self.params)
         model.weight = nn.Parameter(torch.ones((2,2)))
         self.assertEqual(len(model.weights_window), 0)
 
@@ -77,7 +83,13 @@ class TestBuddingLayer(unittest.TestCase):
 
 class TestNeuronBud(unittest.TestCase):
     def setUp(self):
-        self.model = NeuronBud(2, 2, 1, 0.01, [2,2], 'tanh', 'tanh')
+        self.params = {
+            'size_in': 2,
+            'threshold': 0.01,
+            'layers': [2,2],
+            'activation': 'tanh'
+        }
+        self.model = NeuronBud(2, 1, self.params)
 
     def test_init(self):
         self.assertEqual(len(self.model.layerlist), 3)