886e6c60e6c41320718ff607b15f8961bb6c86cb
avamys <dominik.grzegorzewicz@gmail.com>
2021-10-01 18:42:17 +0200

    Include synthetic dataset generation in data script

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,32 +1,45 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import yaml
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 from preprocessing import DataPreprocessor
+from src.data.datasets import DatasetGenerator
 
 
 @click.command()
 @click.argument('dataset')
 @click.argument('input_path', type=click.Path())
 @click.argument('output_filepath', type=click.Path())
-def main(dataset, input_path, output_filepath):
+@click.option('--config_file', '-c', type=click.Path())
+@click.option('--generate', '-g', is_flag=True)
+def main(dataset, input_path, output_filepath, config_file, generate):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data sets from raw data')
-    preprocessor = DataPreprocessor(dataset)
 
-    logger.info('reading data file')
-    X, y = preprocessor.read_data(input_path)
+    if generate:
+        with open(config_file) as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+        random_state = 121
+        dg = DatasetGenerator(**config['base_dataset'], random_state=random_state)
+        ds = dg.get_base()
+        ds.save("data/processed/K0.csv")
+    else:
+        logger.info('making final data sets from raw data')
+        preprocessor = DataPreprocessor(dataset)
 
-    logger.info('processing data')
-    ds = preprocessor.preprocess_data(X, y)
+        logger.info('reading data file')
+        X, y = preprocessor.read_data(input_path)
 
-    logger.info('saving processed data')
-    save_dir = f'{output_filepath}{preprocessor.dataset_name}.csv'
-    ds.save(save_dir)
+        logger.info('processing data')
+        ds = preprocessor.preprocess_data(X, y)
+
+        logger.info('saving processed data')
+        save_dir = f'{output_filepath}/{preprocessor.dataset_name}.csv'
+        ds.save(save_dir)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
