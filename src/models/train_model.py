c7aa7173fc744c726a0263076ac8d682f6960b39
avamys <dominik.grzegorzewicz@gmail.com>
2021-10-01 18:40:24 +0200

    Modify main training script

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -8,7 +8,7 @@ from ray import tune
 from network import Model
 from training import *
 from src.models.config import Configurator
-from src.models.utils import get_criterion
+from src.models.utils import get_criterion, seed_everything
 from src.data.datasets import Dataset
 
 import yaml
@@ -19,16 +19,19 @@ from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
-@click.argument('input_data', type=click.File('rb'))
+@click.argument('input_data', type=click.Path())
 @click.argument('config_file', type=click.Path())
-def main(input_data, config_file):
+@click.option('--baseline', '-b', is_flag=True)
+@click.option('--project', '-p', type=str)
+def main(input_data, config_file, baseline, project):
     """ Runs model training scripts to create and train models on prepared
         datasets (from ../processed) and save them in ../models.
     """
     logger = logging.getLogger(__name__)
     
     # Get reproducible results
-    torch.manual_seed(42)
+    random_seed = 121
+    seed_everything(random_seed)
 
     # Load config dict for running experiment
     with open(config_file) as f:
@@ -36,13 +39,18 @@ def main(input_data, config_file):
 
     logger.info('loading data')
     ds = Dataset.load(input_data)
+    dsid = input_data.strip('/')[-1]
 
     logger.info('running')
 
-    trainer = Configurator(config, ds)
-    analysis = trainer.run()
-
-    print("Best config: ",analysis.get_best_config(metric="accuracy", mode="max"))
+    project = project if project else config['name']
+    trainer = Configurator(
+        config, ds, dataset_id=dsid, enable_wandb=True, random_state=random_seed, project=project)
+    analysis = trainer.run(save_results=True)
+    if baseline:
+        analysis_b = trainer.run(baseline=True, save_results=True)
+        print("Best config (baseline): ",analysis_b.get_best_config(metric="Accuracy_test", mode="max"))
+    print("Best config: ",analysis.get_best_config(metric="Accuracy_test", mode="max"))
 
     # logger.info('saving trained model')
     # save_model(trained_model, 'model1.pth')
