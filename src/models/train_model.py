7297f3bf9a0cad53a7c2da4b0cee7040f358251b
avamys <dominik.grzegorzewicz@gmail.com>
2021-11-06 15:29:22 +0100

    Refactor training script

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,31 +1,24 @@
-import numpy as np
-import torch
-import torch.nn as nn
-from torch.optim import Adam
-from torch.utils.tensorboard import SummaryWriter
-from ray import tune
-
-from network import Model
-from training import *
-from src.models.config import Configurator
-from src.models.utils import get_criterion, seed_everything
-from src.data.datasets import Dataset
-
+import os
 import yaml
 import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+from src.models.config import Configurator
+from src.models.utils import seed_everything
+from src.data.datasets import Dataset, DatasetGenerator
+
 
 @click.command()
-@click.argument('input_data', type=click.Path())
 @click.argument('config_file', type=click.Path())
+@click.option('--input_data', '-i', type=click.Path())
 @click.option('--baseline', '-b', is_flag=True)
 @click.option('--project', '-p', type=str)
-def main(input_data, config_file, baseline, project):
+@click.option('--generate', '-g', is_flag=True)
+def main(config_file, input_data, baseline, project, generate):
     """ Runs model training scripts to create and train models on prepared
-        datasets (from ../processed) and save them in ../models.
+        datasets (from ../processed).
     """
     logger = logging.getLogger(__name__)
     
@@ -37,23 +30,46 @@ def main(input_data, config_file, baseline, project):
     with open(config_file) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-    logger.info('loading data')
-    ds = Dataset.load(input_data)
-    dsid = input_data.strip('/')[-1]
+    # Run on single dataset
+    if input_data:
+        logger.info('loading data')
+        ds = Dataset.load(input_data)
+        dsid = input_data.strip('/')[-1]
+        project = project if project else config['name']
+        trainer = Configurator(config, ds, dataset_id=dsid, enable_wandb=True,
+                               random_state=random_seed, project=project)
 
-    logger.info('running')
+        logger.info('running')
+        analysis = trainer.run(save_results=True)
+        if baseline:
+            analysis_b = trainer.run(baseline=True, save_results=True)
+            print("Best config (baseline): ",analysis_b.get_best_config(metric="Accuracy_test", mode="max"))
+        print("Best config: ",analysis.get_best_config(metric="Accuracy_test", mode="max"))
+    
+    # Run on generated datasets defined in config file
+    if generate:
+        project = project if project else 'capacity-dataset'
+        trainer = Configurator(config, random_state=random_seed, 
+                               project=project, enable_wandb=True)
 
-    project = project if project else config['name']
-    trainer = Configurator(
-        config, ds, dataset_id=dsid, enable_wandb=True, random_state=random_seed, project=project)
-    analysis = trainer.run(save_results=True)
-    if baseline:
-        analysis_b = trainer.run(baseline=True, save_results=True)
-        print("Best config (baseline): ",analysis_b.get_best_config(metric="Accuracy_test", mode="max"))
-    print("Best config: ",analysis.get_best_config(metric="Accuracy_test", mode="max"))
+        dg = DatasetGenerator(**config['base_dataset'], random_state=random_seed)
+        path = dg.generate("data/processed", config)
+        logger.info('all datasets created')
 
-    # logger.info('saving trained model')
-    # save_model(trained_model, 'model1.pth')
+        for dataset in os.listdir(path):
+            ds = Dataset.load(f"{path}/{dataset}")
+            logger.info(f'running on {dataset}')
+            if dataset != 'K0.csv':
+                trainer.project = f'{project}-{dataset[3:5]}'
+            else:
+                trainer.project = f'{project}-base'
+            analysis = trainer.run(dataset=ds, dataset_id=dataset)
+            if baseline:
+                analysis_b = trainer.run(baseline=True, save_results=True)
+                print("Best config (baseline): ",analysis_b.get_best_config(metric="Accuracy_test", mode="max"))
+            print("Best config: ",analysis.get_best_config(metric="Accuracy_test", mode="max"))
+
+    logger.info('task finished')
 
 
 if __name__ == '__main__':
